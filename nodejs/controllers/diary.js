74565ffc0094ec143159ae9888b6f880b5e372b4
Leeseein <huangha607@naver.com>
2024-09-17 16:53:31 +0900

    add flask-address

--- nodejs/controllers/diary.js
+++ nodejs/controllers/diary.js
@@ -6,7 +6,13 @@ const diaryController = async (req, res) => {
     const data = req.body; // 클라이언트가 보낸 데이터를 받아옴
 
     // Python 서버로 POST 요청 보내기
-    const response = await axios.post('파이썬주소', data);
+    const response = await axios.post('http://192.168.0.10:5001/predict', data)
+    .then(response => {
+      console.log('Response from Flask:', response.data);
+    })
+    .catch(error => {
+      console.error('Error sending data to Flask:', error);
+    });
 
     // Python 서버의 응답 데이터를 클라이언트로 전달
     res.json({
